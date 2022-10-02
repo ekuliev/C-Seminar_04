16099f6b291572bbf2b53eab9c0b9686673e4b4e
Elmar_Kouliev <ker1912@mail.ru>
2022-10-02 20:27:18 +0300

    Задача 25

---
+++ Ex4/Program.cs
@@ -0,0 +1,23 @@
+﻿// Задача 25: Напишите цикл, который принимает на вход два числа (A и B) и возводит
+//  число A в натуральную степень B.
+// 3, 5 -> 243 (3⁵)
+// 2, 4 -> 16
+int GetNumber( string k)
+{
+    Console.Write("Input " +k);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+double Pow (double a, double b)
+{
+    double result = a;
+    for (int i=1; i<b; i++)
+    {
+        result = result * a;
+    }
+    return result;
+}
+
+double A = GetNumber("number: ");
+double B = GetNumber("power: ");
+Console.WriteLine (Pow (A, B));
