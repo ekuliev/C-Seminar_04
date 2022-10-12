ef3c9324a98b9059a85658a9fdb8574ed613fc96
Elmar_Kouliev <ker1912@mail.ru>
2022-10-12 23:23:21 +0300

    Задача 47

---
+++ Ex47_FillArray2M/Program.cs
@@ -0,0 +1,21 @@
+﻿// Задача 47. Задайте двумерный массив размером m×n, заполненный случайными вещественными числами.
+// m = 3, n = 4.
+// 0,5 7 -2 -0,2
+// 1 -3,3 8 -9,9
+// 8 7,8 -7,1 9
+// Заполнение массива случайными числами
+
+void FillArray(double[,] matr)
+{
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            matr[i, j] = new Random().Next(-10,10) + new Random().NextDouble();
+            Console.Write(matr[i, j] + " ");
+        }
+        Console.WriteLine();
+    }
+}
+double[,] matrix = new double[3, 4];
+FillArray(matrix);
