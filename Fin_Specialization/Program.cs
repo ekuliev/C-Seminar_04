8f9559c6162cea2eb389665688367383dedbd194
Elmar_Kouliev <ker1912@mail.ru>
2023-03-23 22:18:36 +0300

    Ver 1

---
+++ Fin_Specialization/Program.cs
@@ -0,0 +1,26 @@
+﻿// Из имеющегося массива строк формирует массив из строк, длина которых меньше либо равна 3 символа. 
+string[] array1 = new string[5] {"123", "56", "archivarius", "suck", "ini"};
+string[] array2 = new string[array1.Length];
+void CreateSecondArray(string[] array1, string[] array2)
+{
+    int n = 0;
+    for (int i = 0; i < array1.Length; i++)
+    {
+    if(array1[i].Length <= 3)
+        {
+        array2[n] = array1[i];
+        n++;
+        }
+    }
+}
+void PrintArray(string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
+CreateSecondArray(array1, array2);
+PrintArray(array1);
+PrintArray(array2);
